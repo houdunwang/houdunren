6e4e7d816e4133a687ad8944212c992e0f1e89c8
houdunwang <2300071698@qq.com>
2019-03-02 23:12:30 +0800

    生成更新列表

---
+++ updateLists/1551539549.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'build' => 1551539549,
+  'total' => 1,
+  'logs' => 
+  array (
+    0 => '菜单细化',
+  ),
+  'files' => 
+  array (
+    'app/Console/Commands/CmsCreateUpdateFile.php' => 'M',
+  ),
+);
