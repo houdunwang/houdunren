026d9fbc7cade65fd1e588d9d955c48b8eb7ae07
houdunwang <2300071698@qq.com>
2019-03-04 21:22:12 +0800

    生成更新列表

--- app/Console/Commands/CmsCreateUpdateFile.php
+++ app/Console/Commands/CmsCreateUpdateFile.php
@@ -18,9 +18,6 @@ class CmsCreateUpdateFile extends Command
     {
         $build = time();
         put_contents_file('version.php', ['build' => $build]);
-        exec("git add");
-        exec("git commit -m '生成编译版本号'");
-        exec("git push");
         exec("git diff master dev --name-status", $files);
         exec('git log dev ^master --pretty=format:"%s"', $logs);
         \Storage::disk('base')->makeDirectory('updateLists');
