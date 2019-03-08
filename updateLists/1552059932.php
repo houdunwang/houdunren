269ba3c577406c666a2e66f2a06e376cefe03567
houdunwang <2300071698@qq.com>
2019-03-08 23:45:33 +0800

    新版发布

---
+++ updateLists/1552059932.php
@@ -0,0 +1,15 @@
+<?php return array (
+  'build' => 1552059932,
+  'total' => 4,
+  'logs' => 
+  array (
+    0 => '优化本地更新',
+  ),
+  'files' => 
+  array (
+    '.gitignore' => 'M',
+    'app/Http/Controllers/Update/SystemController.php' => 'M',
+    'app/Servers/HttpServer.php' => 'M',
+    'cmsupdate.sh' => 'M',
+  ),
+);
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php return array (
-  'build' => 1552053407,
+  'build' => 1552059932,
 );
