0e914cb077d8d38df0232fb61e4cad3e83c6e5b8
houdunwang <2300071698@qq.com>
2019-03-13 03:18:01 +0800

    新版发布

---
+++ updateLists/1552418280.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'build' => 1552418280,
+  'total' => 2,
+  'logs' => 
+  array (
+    0 => '静态数据改为相对地址',
+  ),
+  'files' => 
+  array (
+    'public/mix-manifest.json' => 'M',
+    'resources/views/site/config/_info.blade.php' => 'M',
+  ),
+);
