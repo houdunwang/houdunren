c2b257ab137a2e9621831c3c743709f02596824a
houdunwang <2300071698@qq.com>
2019-03-14 07:19:23 +0800

    新版发布

---
+++ updateLists/1552519162.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'build' => 1552519162,
+  'total' => 1,
+  'logs' => 
+  array (
+    0 => '更新跳转逻辑',
+  ),
+  'files' => 
+  array (
+    'resources/views/update/module/update.blade.php' => 'M',
+  ),
+);
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php return array (
-  'build' => 1552518733,
+  'build' => 1552519162,
 );
