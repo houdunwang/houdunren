9a905b851589878ffba262408b8db1a1c4a88e52
houdunwang <2300071698@qq.com>
2019-03-14 23:29:30 +0800

    新版发布

---
+++ updateLists/1552577370.php
@@ -0,0 +1,18 @@
+<?php return array (
+  'build' => 1552577370,
+  'total' => 4,
+  'logs' => 
+  array (
+    0 => '云主机默认使用HTTPS协议',
+    1 => '云主机默认使用HTTPS协议',
+    2 => '网站维护允许站长与系统管理员访问',
+    3 => '网站维护允许站长与系统管理员访问',
+  ),
+  'files' => 
+  array (
+    'app/Http/Middleware/FrontMiddleware.php' => 'M',
+    'config/app.php' => 'M',
+    'config/purifier.php' => 'M',
+    'resources/views/system/cloud/create.blade.php' => 'M',
+  ),
+);
