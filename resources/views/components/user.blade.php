096f5da1ad32cb32474fa4a8ced0d634e813d1a7
houdunwang <2300071698@qq.com>
2019-03-16 02:41:55 +0800

    修复个人空间链接

--- resources/views/components/user.blade.php
+++ resources/views/components/user.blade.php
@@ -22,27 +22,28 @@
             <i class="fa fa-qq {{active_class($user['qq'],'text-info')}}"></i>
         </a>
     </div>
-    <div class="text-center pb-3">
-        @if ($user->hasFans(auth()->user()))
-            <a href="{{route('member.follow.make',$user)}}" class="btn btn-outline-success btn-sm">
-                <i class="fa fa-plus"></i> 关注 TA
-            </a>
-        @else
-            <a href="{{route('member.follow.make',$user)}}" class="btn btn-success btn-sm">
-                <i class="fa fa-plus"></i> 已关注
-            </a>
-        @endif
-        @if (auth()->user()->isFavour())
-            <a href="{{route('member.favour.make',['model'=>'App-User','id'=>$user['id']])}}"
-               class="btn btn-info btn-sm">
-                <i class="fa fa-heart"></i> 感谢点赞
-            </a>
-        @else
-            <a href="{{route('member.favour.make',['model'=>'App-User','id'=>$user['id']])}}"
-               class="btn btn-outline-info btn-sm">
-                <i class="fa fa-heart-o"></i> 点个赞呗
-            </a>
-        @endif
-
-    </div>
+    @auth
+        <div class="text-center pb-3">
+            @if ($user->hasFans(auth()->user()))
+                <a href="{{route('member.follow.make',$user)}}" class="btn btn-outline-success btn-sm">
+                    <i class="fa fa-plus"></i> 关注 TA
+                </a>
+            @else
+                <a href="{{route('member.follow.make',$user)}}" class="btn btn-success btn-sm">
+                    <i class="fa fa-plus"></i> 已关注
+                </a>
+            @endif
+            @if (auth()->user()->isFavour())
+                <a href="{{route('member.favour.make',['model'=>'App-User','id'=>$user['id']])}}"
+                   class="btn btn-info btn-sm">
+                    <i class="fa fa-heart"></i> 感谢点赞
+                </a>
+            @else
+                <a href="{{route('member.favour.make',['model'=>'App-User','id'=>$user['id']])}}"
+                   class="btn btn-outline-info btn-sm">
+                    <i class="fa fa-heart-o"></i> 点个赞呗
+                </a>
+            @endif
+        </div>
+    @endauth
 </div>
