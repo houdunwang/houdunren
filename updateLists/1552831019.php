cb557a8ddee49147cb0211b45af0fc5aa38569b4
houdunwang <2300071698@qq.com>
2019-03-17 21:56:59 +0800

    新版发布

---
+++ updateLists/1552831019.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'build' => 1552831019,
+  'total' => 2,
+  'logs' => 
+  array (
+    0 => '移除登录站点检测',
+  ),
+  'files' => 
+  array (
+    'app/Http/Controllers/System/SiteController.php' => 'M',
+    'app/Repositories/UserRepository.php' => 'M',
+  ),
+);
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php return array (
-  'build' => 1552829149,
+  'build' => 1552831019,
 );
