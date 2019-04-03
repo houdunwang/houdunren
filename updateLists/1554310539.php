f026379359ee1fbfb2d75e58229d96ae1bc651f3
houdunwang <2300071698@qq.com>
2019-04-04 00:55:40 +0800

    新版发布

---
+++ updateLists/1554310539.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'build' => 1554310539,
+  'total' => 2,
+  'logs' => 
+  array (
+    0 => '修复部分服务器绑定云帐号失败问题',
+  ),
+  'files' => 
+  array (
+    'app/Http/Controllers/System/CloudController.php' => 'M',
+    'app/Servers/HttpServer.php' => 'M',
+  ),
+);
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php return array (
-  'build' => 1554309076,
+  'build' => 1554310539,
 );
