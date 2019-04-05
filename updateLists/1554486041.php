6f76c3b601c5007f4556abdafe7ae0063f27f5a5
houdunwang <2300071698@qq.com>
2019-04-06 01:40:42 +0800

    新版发布

---
+++ updateLists/1554486041.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'build' => 1554486041,
+  'total' => 1,
+  'logs' => 
+  array (
+    0 => '阶段完成',
+  ),
+  'files' => 
+  array (
+    'cmsupdate.sh' => 'M',
+  ),
+);
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php return array (
-  'build' => 1554485982,
+  'build' => 1554486041,
 );
