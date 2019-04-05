15269b33692e66035fa865b5e66320c2c7fd96d5
houdunwang <2300071698@qq.com>
2019-04-06 01:40:36 +0800

    阶段完成

--- cmsupdate.sh
+++ cmsupdate.sh
@@ -5,10 +5,12 @@ git push
 php artisan cms:create-update-file
 git add .
 git commit -m '新版发布'
-gp
+git push
+git push gitee
 git checkout master
 git merge dev
-gp
+git push
+git push gitee
 git checkout dev
 time=$(date "+%Y%m%d%H%M")
 git archive --format zip --output public/zips/HDCMS-${time}.zip master
