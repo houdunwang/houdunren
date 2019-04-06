1554dcf3d0d5c2ec983448d34166d91687a9d8b7
houdunwang <2300071698@qq.com>
2019-04-06 21:37:47 +0800

    新版发布

---
+++ updateLists/1554557867.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'build' => 1554557867,
+  'total' => 10,
+  'logs' => 
+  array (
+    0 => '优化个人空间',
+  ),
+  'files' => 
+  array (
+    'app/Http/Controllers/User/HomeController.php' => 'M',
+    'app/Repositories/FavoriteRepository.php' => 'M',
+    'public/mix-manifest.json' => 'M',
+    'resources/views/components/user.blade.php' => 'M',
+    'resources/views/components/user_icon.blade.php' => 'M',
+    'resources/views/user/fans.blade.php' => 'M',
+    'resources/views/user/follower.blade.php' => 'M',
+    'resources/views/user/layouts/menu.blade.php' => 'M',
+    'resources/views/user/layouts/pc.blade.php' => 'M',
+    'routes/web/user.php' => 'M',
+  ),
+);
--- version.php
+++ version.php
@@ -1,3 +1,3 @@
 <?php return array (
-  'build' => 1554492930,
+  'build' => 1554557867,
 );
