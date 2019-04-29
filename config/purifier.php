d453cbfb623ced2a0072e033dc2a8abf2eac0a38
houdunwang <2300071698@qq.com>
2019-04-29 21:55:35 +0800

    优化标签过滤机制

--- config/purifier.php
+++ config/purifier.php
@@ -24,7 +24,7 @@ return [
     'settings' => [
         'default' => [
             'HTML.Doctype' => 'HTML 4.01 Transitional',
-            'HTML.Allowed' => 'div,b,strong,i,em,u,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
+            'HTML.Allowed' => 'table,tr,td,th,thead,tbody,div,b,strong,i,em,pre,code,u,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src],h1,h2,h3,h4,h5,h6',
             'CSS.AllowedProperties' => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
             'AutoFormat.AutoParagraph' => true,
             'AutoFormat.RemoveEmpty' => true,
