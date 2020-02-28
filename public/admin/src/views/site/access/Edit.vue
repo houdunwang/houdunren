1b6364e727c8264cfa5e6c8fca0a3fefabe1f2ce
houdunren.com <2300071698@qq.com>
2020-02-29 01:32:14 +0800

    菜单包含子菜单

--- public/admin/src/views/site/access/Edit.vue
+++ public/admin/src/views/site/access/Edit.vue
@@ -10,18 +10,31 @@
       >操作员管理</router-link>
       <a class="nav-link active" href="#">权限设置</a>
     </nav>
-    <div class="card mt-2" v-for="(menus, moduleName) in menus" :key="moduleName">
-      <div class="card-header">{{ moduleName }}</div>
-      <div class="card-body">
-        <el-checkbox-group v-model="permissions">
-          <div class="row">
-            <div class="col-6 col-md-2" v-for="menu in menus" :key="menu.permission">
-              <el-checkbox :label="menu.permission">{{ menu.title }}</el-checkbox>
+
+    <el-checkbox-group v-model="permissions">
+      <div class="card mb-2 shadow-sm" v-for="module in modules" :key="module.model.id">
+        <div class="card-header bg-light">{{ module.config.title }}</div>
+        <div class="card-body bg-light">
+          <div v-for="category in module.menu.admin" :key="category.category.title">
+            <div
+              class="mb-2 p-2 border bg-light shadow-sm"
+              v-for="menu in category.menus"
+              :key="menu.group.title"
+            >
+              <h6 class="text-secondary">
+                <i class="fa fa-th-list" aria-hidden="true"></i>
+                {{menu.group.title}}
+              </h6>
+              <div class="row">
+                <div class="col-6 col-md-3" v-for="item in menu.items" :key="item.title">
+                  <el-checkbox :label="item.permission">{{ item.title }}</el-checkbox>
+                </div>
+              </div>
             </div>
           </div>
-        </el-checkbox-group>
+        </div>
       </div>
-    </div>
+    </el-checkbox-group>
     <button class="btn btn-primary mt-3" @click="submit">保存权限</button>
   </div>
 </template>
@@ -30,7 +43,7 @@
 export default {
   data() {
     return {
-      menus: [],
+      modules: [],
       //用户拥有的权限
       permissions: []
     }
@@ -38,10 +51,10 @@ export default {
   async created() {
     const params = this.$route.params
     let response = await Promise.all([
-      this.axios.get(`site/${params.sid}/permission-menu`).then(r => r.data.data),
+      this.axios.get(`site/${params.sid}/module/site`).then(r => r.data.data),
       this.axios.get(`site/${params.sid}/access/${params.uid}`).then(r => r.data.data)
     ])
-    this.$set(this, 'menus', response[0])
+    this.$set(this, 'modules', response[0])
     this.$set(
       this,
       'permissions',
@@ -60,4 +73,17 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-checkbox-group {
+  font-size: inherit;
+}
+h6::before {
+  // content: '';
+  display: inline-block;
+  position: relative;
+  border-left: solid 3px #ddd;
+  height: 15px;
+  top: 2px;
+  margin-right: 5px;
+}
+</style>
