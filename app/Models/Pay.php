0d337f8ca80d4a9a9c17dd04a5d4f603c9609df6
houdunren <2300071698@qq.com>
2021-03-24 19:21:47 +0800

    优化文件结构

--- app/Models/Pay.php
+++ app/Models/Pay.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 支付
+ * @package App\Models
+ */
 class Pay extends Model
 {
     protected $fillable = ['site_id', 'module_id', 'user_id', 'price', 'subject', 'sn', 'type', 'status'];
